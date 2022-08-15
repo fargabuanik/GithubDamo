14b67a8f64a858494994560ca0f7f7ee9abbc453
fargabuanik <fargabanik@gmail.com>
2022-08-15 16:10:34 -0400

    updata

---
+++ src/githubDemo/HelloGitHub.java
@@ -0,0 +1,11 @@
+package githubDemo;
+
+/*Create a project --- "GithubDemo". Create a package name -- "githubDemo". Create a class --- "HelloGitHub". Inside the class, type main method and under it print "I know how to create a separate remote repository in Github and can push my project GithubDemo, without help of anyone". Organize code, avoid white space and push the project to Github. Paste the link below without the help of anyone else.*/
+
+public class HelloGitHub {
+	public static void main(String[] args) {
+		System.out.println(" know how to create a separate remote repository in Github and can push my project GithubDemo, without help of anyone");
+		
+	}
+
+}
